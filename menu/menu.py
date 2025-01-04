343eb1f676bf7d32bcb10127a688286ae07c9270
Akbar1007 <a.akbarjon007@gmail.com>
2025-01-04 11:50:52 +0500

    Added json for menu items

--- menu/menu.py
+++ menu/menu.py
@@ -1,3 +1,5 @@
+import json
+
 class MenuItem:
     """Models each Menu Item."""
     def __init__(self, name, water, milk, coffee, cost):
@@ -12,21 +14,46 @@ class MenuItem:
 
 class Menu:
     """Models the Menu with drinks."""
-    def __init__(self):
-        self.menu = [
-            MenuItem(name="latte", water=200, milk=150, coffee=24, cost=2.5),
-            MenuItem(name="espresso", water=50, milk=0, coffee=18, cost=1.5),
-            MenuItem(name="cappuccino", water=250, milk=50, coffee=24, cost=3),
-            MenuItem(name="americano", water=300, milk=0, coffee=24, cost=2),
-            MenuItem(name="macchiato", water=200, milk=50, coffee=24, cost=2.5),
-            MenuItem(name="flat white", water=150, milk=150, coffee=18, cost=2.5),
-            MenuItem(name="ristretto", water=30, milk=0, coffee=18, cost=1.5),
-            MenuItem(name="lungo", water=70, milk=0, coffee=18, cost=1.5)
-        ]
+    def __init__(self, menu_file="python/menu/menu.json"):
+        self.menu = []
+        self.load_menu(menu_file)
+
+        # self.menu = [
+        #     MenuItem(name="latte", water=200, milk=150, coffee=24, cost=2.5),
+        #     MenuItem(name="espresso", water=50, milk=0, coffee=18, cost=1.5),
+        #     MenuItem(name="cappuccino", water=250, milk=50, coffee=24, cost=3),
+        #     MenuItem(name="americano", water=300, milk=0, coffee=24, cost=2),
+        #     MenuItem(name="macchiato", water=200, milk=50, coffee=24, cost=2.5),
+        #     MenuItem(name="flat white", water=150, milk=150, coffee=18, cost=2.5),
+        #     MenuItem(name="ristretto", water=30, milk=0, coffee=18, cost=1.5),
+        #     MenuItem(name="lungo", water=70, milk=0, coffee=18, cost=1.5)
+        # ]
+
+    def load_menu(self, menu_file):
+        """Loads menu items from a JSON file."""
+        try:
+            with open(menu_file, "r") as file:
+                items = json.load(file)
+                for item in items:
+                    self.menu.append(MenuItem(
+                        name=item["name"],
+                        water=item["water"],
+                        milk=item["milk"],
+                        coffee=item["coffee"],
+                        cost=item["cost"]
+                    ))
+        except FileNotFoundError:
+            print(f"Error: The file '{menu_file}' was not found.")
+        # except json.JSONDecodeError:
+        #     print(f"Error: The file '{menu_file}' contains invalid JSON.")
+
 
     def get_items(self):
         """Returns all the names of the available menu items"""
+        if not self.menu:
+            print("Menu is empty!")
         options = ""
+
         for item in self.menu:
             options += f"${item.cost} - {item.name}/"
         return options
